c54fe0e8bbed96f6b89d5d153229a9a9a24d7e77
sile <iamsai007@sina.com>
2013-12-11 20:00:43 +0800

    Initial Commit

---
+++ BM algorithm/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  BM algorithm
+//
+//  Created by taowei on 13-12-11.
+//  Copyright (c) 2013年 taowei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
